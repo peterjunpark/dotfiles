27dc8c1b04d97128c2ef1f68835e44e01d5267c8
Peter Park <peter.park@amd.com>
2024-09-09 13:20:43 -0400

    update nvim plugins

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
     local oil = require 'oil'
     oil.setup {
       default_file_explorer = true,
+      keymaps = {
+        ['q'] = 'actions.close'
+      },
     }
 
     vim.keymap.set('n', '<leader>e', oil.toggle_float, { desc = 'Oil' })
