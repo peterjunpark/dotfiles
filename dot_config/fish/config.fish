ec15efb6b6199803b8d790cfda506133f3d7eb8f
Peter Park <peter.park@amd.com>
2024-09-10 16:03:16 -0400

    add yazi cmd

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -14,3 +14,13 @@ zoxide init fish | source
 # added by Webi for pyenv
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
+
+# yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
