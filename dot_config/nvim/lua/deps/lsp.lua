359114432d0e2861c70c9033e74075f7ac22220b
Peter Park <park@hey.com>
2024-10-22 16:33:03 -0400

    add elixir

--- dot_config/nvim/lua/deps/lsp.lua
+++ dot_config/nvim/lua/deps/lsp.lua
@@ -14,6 +14,35 @@ require('lazydev').setup {
 }
 
 add {
+	source = 'elixir-tools/elixir-tools.nvim',
+	depends = {
+		'nvim-lua/plenary.nvim',
+	},
+}
+
+local elixir = require 'elixir'
+local elixirls = require 'elixir.elixirls'
+
+elixir.setup {
+	nextls = { enable = true },
+	elixirls = {
+		enable = true,
+		settings = elixirls.settings {
+			dialyzerEnabled = false,
+			enableTestLenses = false,
+		},
+		-- on_attach = function(client, bufnr)
+		-- 	vim.keymap.set('n', '<>ap', ':ElixirFromPipe<cr>', { buffer = true, noremap = true })
+		-- 	vim.keymap.set('n', '<space>tp', ':ElixirToPipe<cr>', { buffer = true, noremap = true })
+		-- 	vim.keymap.set('v', '<space>em', ':ElixirExpandMacro<cr>', { buffer = true, noremap = true })
+		-- end,
+	},
+	projectionist = {
+		enable = true,
+	},
+}
+
+add {
 	source = 'neovim/nvim-lspconfig',
 	depends = {
 		'williamboman/mason.nvim',
